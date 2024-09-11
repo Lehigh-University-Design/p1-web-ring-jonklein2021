0f2f9ec0b663291262b3fe52f75e6e0582682cfc
Jon Klein <jonklein2021@gmail.com>
2024-09-11 09:58:45 -0400

    add more links

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function visitRandom() {
     const sites = [
+        'https://lehigh-university-design.github.io/p1-web-ring-sjm325/',
+        'https://lehigh-university-design.github.io/jp-web-ring/',
+        'https://lehigh-university-design.github.io/p1-web-ring-mls325/',
+        'https://lehigh-university-design.github.io/p1-web-ring-paleduck/',
+        'https://lehigh-university-design.github.io/p1-web-ring-DustinBraufman/',
+        'https://lehigh-university-design.github.io/p1-web-ring-madisynbabb/',
+        'https://lehigh-university-design.github.io/p1-web-ring-KaitMarchiony/',
+        'https://lehigh-university-design.github.io/p1-web-ring-camila-des/',
+        'https://lehigh-university-design.github.io/p1-web-ring-codingwithamy/',
+        'https://lehigh-university-design.github.io/p1-web-ring-Renach/',
+        'https://lehigh-university-design.github.io/p1-web-ring-marincormier/',
+        'https://lehigh-university-design.github.io/p1-web-ring-maribritto13/',
         'https://lehigh-university-design.github.io/p1-web-ring-amandamertz14/',
         'https://lehigh-university-design.github.io/p1-web-ring-jonklein2021/',
         'https://lehigh-university-design.github.io/p1-web-ring-felicityace/',
-        'https://lehigh-university-design.github.io/p1-web-ring-madisynbabb/',
-        'https://lehigh-university-design.github.io/p1-web-ring-mls325/',
-        'https://lehigh-university-design.github.io/p1-web-ring-paleduck/',
-        'https://lehigh-university-design.github.io/p1-web-ring-DustinBraufman/'
     ]
     window.location.href = sites[Math.floor(Math.random() * sites.length)];
 }
